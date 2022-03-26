80c0e333dba8292eca883ec6ca85bea1f30726c3
Cybernasty <93290097+Cybernasty@users.noreply.github.com>
2022-03-26 12:21:39 +0100

    Add files via upload

---
+++ audio/Audio2.py
@@ -0,0 +1,13 @@
+from vidstream import AudioReceiver
+from vidstream import AudioSender
+import threading
+
+
+receiver = AudioReceiver = AudioReceiver('192.168.1.4',5555)
+receive_thread = threading.Thread(target=receiver.start_server)
+
+sender = AudioSender('192.168.1.4',6666)
+sender_thread = threading.Thread(target=sender.start_stream)
+
+receive_thread.start()
+sender_thread.start()
